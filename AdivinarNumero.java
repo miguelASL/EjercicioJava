223764995fba08f3d458df28b88714aea15fecca
Miguel Ángel Sarmiento Levy <112961729+miguelASL@users.noreply.github.com>
2023-06-19 14:41:54 +0200

    Add files via upload

---
+++ AdivinarNumero.java
@@ -0,0 +1,27 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class AdivinarNumero {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int prediccion = 0;
+
+        int numero = (int)(Math.random()* 100) + 1;
+
+        while (prediccion != numero) {
+            System.out.print("Di el numero: ");
+            prediccion = s.nextInt();
+
+            if(prediccion > numero) {
+                System.out.println("Di un numero mas pequeño. Sigue intentandolo");
+            } else if (prediccion == numero) {
+                System.out.println("Correcto, tu numero: " + prediccion + " y " + "El numero aleatorio : " +
+                        numero + " Son iguales." );
+            }else {
+                System.out.println("Di uno mas grande, Sigue intentandolo");
+            }
+        }
+
+    }
+}
