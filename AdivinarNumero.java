092b84fe070d3e9603c2eb53adabb4f7e432445f
Miguel Ángel Sarmiento Levy <msarmientolevy@gmail.com>
2023-06-27 15:42:10 +0200

    Cambio en el nombre
    Este cambio era de prueba

--- AdivinarNumero.java
+++ AdivinarNumero.java
@@ -2,7 +2,7 @@ package ejercicios;
 
 import java.util.Scanner;
 
-public class AdivinarNumero {
+public class AdivinarNumeros {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int prediccion = 0;
